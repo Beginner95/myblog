5eb47148f36f2fb99c87a9643ed90b52f799b5dd
Vaharsolta <vaha-22@mail.ru>
2018-03-15 17:15:06 +0300

    Добавлен модуль админки

--- config/web.php
+++ config/web.php
@@ -52,6 +52,13 @@ $config = [
         ],
         
     ],
+
+    'modules' => [
+        'admin' => [
+            'class' => 'app\modules\admin\Module',
+        ],
+    ],
+
     'params' => $params,
 ];
 
