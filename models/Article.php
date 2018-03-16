df5c75b70a3f613e0352632520524cdb8c810f92
Vaharsolta <vaha-22@mail.ru>
2018-03-16 18:48:33 +0300

    Добавлена валидация данных статьи

--- models/Article.php
+++ models/Article.php
@@ -37,10 +37,11 @@ class Article extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['description', 'content'], 'string'],
-            [['date'], 'safe'],
-            [['viewed', 'user_id', 'status', 'category_id'], 'integer'],
-            [['title', 'image'], 'string', 'max' => 255],
+            [['title'], 'required'],
+            [['title', 'description', 'content'], 'string'],
+            [['date'], 'date', 'format' => 'php:Y-m-d'],
+            [['date'], 'default', 'value' => date('Y-m-d')],
+            [['title'], 'string', 'max' => 255]
         ];
     }
 
