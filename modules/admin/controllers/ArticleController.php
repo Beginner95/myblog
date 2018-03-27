432a09b1fd8b3898affa0248fb8dfc26cb8c6c86
Vaharsolta <vaha-22@mail.ru>
2018-03-27 22:04:48 +0300

    Реализован action по установке картинки для статьи

--- modules/admin/controllers/ArticleController.php
+++ modules/admin/controllers/ArticleController.php
@@ -2,12 +2,14 @@
 
 namespace app\modules\admin\controllers;
 
+use app\models\ImageUpload;
 use Yii;
 use app\models\Article;
 use app\models\ArticleSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\web\UploadedFile;
 
 /**
  * ArticleController implements the CRUD actions for Article model.
@@ -123,5 +125,22 @@ class ArticleController extends Controller
         }
 
         throw new NotFoundHttpException('The requested page does not exist.');
+    }
+
+    public function actionSetImage($id)
+    {
+        $model = new ImageUpload();
+
+        if (Yii::$app->request->isPost) {
+            $article = $this->findModel($id);
+
+            $file = UploadedFile::getInstance($model, 'image');
+
+            if (true === $article->saveImage($model->uploadFile($file, $article->image))) {
+                return $this->redirect(['view', 'id' => $article->id]);
+            }
+        }
+
+        return $this->render('image', ['model' => $model]);
     }
 }
