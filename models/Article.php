cdebf5ccc68fb13b218d5ca7f708b1172646d1e6
Vaharsolta <vaha-22@mail.ru>
2018-03-28 20:03:14 +0300

    Реализован метод по сохранению выбраннок категории для статьи

--- models/Article.php
+++ models/Article.php
@@ -94,4 +94,13 @@ class Article extends \yii\db\ActiveRecord
         return $this->hasOne(Category::className(), ['id' => 'category_id']);
     }
 
+    public function saveCategory($category_id)
+    {
+        $category = Category::findOne($category_id);
+        if ($category != null) {
+            $this->link('category', $category);
+            return true;
+        }
+    }
+
 }
