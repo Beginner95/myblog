4e2ba62eb0035f57cb39d4eff0b75409fa72573d
Vaharsolta <vaha-22@mail.ru>
2018-10-30 08:09:27 +0300

    Обновил yii2

--- modules/admin/controllers/ArticleController.php
+++ modules/admin/controllers/ArticleController.php
@@ -105,6 +105,8 @@ class ArticleController extends Controller
      * @param integer $id
      * @return mixed
      * @throws NotFoundHttpException if the model cannot be found
+     * @throws \Throwable
+     * @throws \yii\db\StaleObjectException
      */
     public function actionDelete($id)
     {
