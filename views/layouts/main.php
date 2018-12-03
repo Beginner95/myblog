3670186615bd5d4d0e140898fc6c024c14e33ce0
Vaharsolta <vaha-22@mail.ru>
2018-12-03 13:24:27 +0300

    Перенес структуру шаблона

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -8,9 +8,9 @@ use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
-use app\assets\AppAsset;
+use app\assets\PublicAsset;
 
-AppAsset::register($this);
+PublicAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -26,54 +26,308 @@ AppAsset::register($this);
 <body>
 <?php $this->beginBody() ?>
 
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
-        ],
-    ]);
-    NavBar::end();
-    ?>
 
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+
+<!-- Wrapper -->
+<div id="wrapper">
+
+    <!-- Header -->
+    <header id="header">
+        <h1><a href="index.html">Future Imperfect</a></h1>
+        <nav class="links">
+            <ul>
+                <li><a href="#">Lorem</a></li>
+                <li><a href="#">Ipsum</a></li>
+                <li><a href="#">Feugiat</a></li>
+                <li><a href="#">Tempus</a></li>
+                <li><a href="#">Adipiscing</a></li>
+            </ul>
+        </nav>
+        <nav class="main">
+            <ul>
+                <li class="search">
+                    <a class="fa-search" href="#search">Search</a>
+                    <form id="search" method="get" action="#">
+                        <input type="text" name="query" placeholder="Search" />
+                    </form>
+                </li>
+                <li class="menu">
+                    <a class="fa-bars" href="#menu">Menu</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- Menu -->
+    <section id="menu">
+
+        <!-- Search -->
+        <section>
+            <form class="search" method="get" action="#">
+                <input type="text" name="query" placeholder="Search" />
+            </form>
+        </section>
+
+        <!-- Links -->
+        <section>
+            <ul class="links">
+                <li>
+                    <a href="#">
+                        <h3>Lorem ipsum</h3>
+                        <p>Feugiat tempus veroeros dolor</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <h3>Dolor sit amet</h3>
+                        <p>Sed vitae justo condimentum</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <h3>Feugiat veroeros</h3>
+                        <p>Phasellus sed ultricies mi congue</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <h3>Etiam sed consequat</h3>
+                        <p>Porta lectus amet ultricies</p>
+                    </a>
+                </li>
+            </ul>
+        </section>
+
+        <!-- Actions -->
+        <section>
+            <ul class="actions stacked">
+                <li><a href="#" class="button large fit">Log In</a></li>
+            </ul>
+        </section>
+
+    </section>
+
+    <!-- Main -->
+    <div id="main">
+
+        <!-- Post -->
+        <article class="post">
+            <header>
+                <div class="title">
+                    <h2><a href="single.html">Magna sed adipiscing</a></h2>
+                    <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
+                </div>
+                <div class="meta">
+                    <time class="published" datetime="2015-11-01">November 1, 2015</time>
+                    <a href="#" class="author"><span class="name">Jane Doe</span><img src="public/images/avatar.jpg" alt="" /></a>
+                </div>
+            </header>
+            <a href="single.html" class="image featured"><img src="public/images/pic01.jpg" alt="" /></a>
+            <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+            <footer>
+                <ul class="actions">
+                    <li><a href="single.html" class="button large">Continue Reading</a></li>
+                </ul>
+                <ul class="stats">
+                    <li><a href="#">General</a></li>
+                    <li><a href="#" class="icon fa-heart">28</a></li>
+                    <li><a href="#" class="icon fa-comment">128</a></li>
+                </ul>
+            </footer>
+        </article>
+
+        <!-- Post -->
+        <article class="post">
+            <header>
+                <div class="title">
+                    <h2><a href="single.html">Ultricies sed magna euismod enim vitae gravida</a></h2>
+                    <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
+                </div>
+                <div class="meta">
+                    <time class="published" datetime="2015-10-25">October 25, 2015</time>
+                    <a href="#" class="author"><span class="name">Jane Doe</span><img src="public/images/avatar.jpg" alt="" /></a>
+                </div>
+            </header>
+            <a href="single.html" class="image featured"><img src="public/images/pic02.jpg" alt="" /></a>
+            <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper.</p>
+            <footer>
+                <ul class="actions">
+                    <li><a href="single.html" class="button large">Continue Reading</a></li>
+                </ul>
+                <ul class="stats">
+                    <li><a href="#">General</a></li>
+                    <li><a href="#" class="icon fa-heart">28</a></li>
+                    <li><a href="#" class="icon fa-comment">128</a></li>
+                </ul>
+            </footer>
+        </article>
+
+        <!-- Post -->
+        <article class="post">
+            <header>
+                <div class="title">
+                    <h2><a href="single.html">Euismod et accumsan</a></h2>
+                    <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
+                </div>
+                <div class="meta">
+                    <time class="published" datetime="2015-10-22">October 22, 2015</time>
+                    <a href="#" class="author"><span class="name">Jane Doe</span><img src="public/images/avatar.jpg" alt="" /></a>
+                </div>
+            </header>
+            <a href="single.html" class="image featured"><img src="public/images/pic03.jpg" alt="" /></a>
+            <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla. Cras vehicula tellus eu ligula viverra, ac fringilla turpis suscipit. Quisque vestibulum rhoncus ligula.</p>
+            <footer>
+                <ul class="actions">
+                    <li><a href="single.html" class="button large">Continue Reading</a></li>
+                </ul>
+                <ul class="stats">
+                    <li><a href="#">General</a></li>
+                    <li><a href="#" class="icon fa-heart">28</a></li>
+                    <li><a href="#" class="icon fa-comment">128</a></li>
+                </ul>
+            </footer>
+        </article>
+
+        <!-- Pagination -->
+        <ul class="actions pagination">
+            <li><a href="" class="disabled button large previous">Previous Page</a></li>
+            <li><a href="#" class="button large next">Next Page</a></li>
+        </ul>
+
     </div>
+
+    <!-- Sidebar -->
+    <section id="sidebar">
+
+        <!-- Intro -->
+        <section id="intro">
+            <a href="#" class="logo"><img src="public/images/logo.jpg" alt="" /></a>
+            <header>
+                <h2>Future Imperfect</h2>
+                <p>Another fine responsive site template by <a href="http://html5up.net">HTML5 UP</a></p>
+            </header>
+        </section>
+
+        <!-- Mini Posts -->
+        <section>
+            <div class="mini-posts">
+
+                <!-- Mini Post -->
+                <article class="mini-post">
+                    <header>
+                        <h3><a href="single.html">Vitae sed condimentum</a></h3>
+                        <time class="published" datetime="2015-10-20">October 20, 2015</time>
+                        <a href="#" class="author"><img src="public/images/avatar.jpg" alt="" /></a>
+                    </header>
+                    <a href="single.html" class="image"><img src="public/images/pic04.jpg" alt="" /></a>
+                </article>
+
+                <!-- Mini Post -->
+                <article class="mini-post">
+                    <header>
+                        <h3><a href="single.html">Rutrum neque accumsan</a></h3>
+                        <time class="published" datetime="2015-10-19">October 19, 2015</time>
+                        <a href="#" class="author"><img src="public/images/avatar.jpg" alt="" /></a>
+                    </header>
+                    <a href="single.html" class="image"><img src="public/images/pic05.jpg" alt="" /></a>
+                </article>
+
+                <!-- Mini Post -->
+                <article class="mini-post">
+                    <header>
+                        <h3><a href="single.html">Odio congue mattis</a></h3>
+                        <time class="published" datetime="2015-10-18">October 18, 2015</time>
+                        <a href="#" class="author"><img src="public/images/avatar.jpg" alt="" /></a>
+                    </header>
+                    <a href="single.html" class="image"><img src="public/images/pic06.jpg" alt="" /></a>
+                </article>
+
+                <!-- Mini Post -->
+                <article class="mini-post">
+                    <header>
+                        <h3><a href="single.html">Enim nisl veroeros</a></h3>
+                        <time class="published" datetime="2015-10-17">October 17, 2015</time>
+                        <a href="#" class="author"><img src="public/images/avatar.jpg" alt="" /></a>
+                    </header>
+                    <a href="single.html" class="image"><img src="public/images/pic07.jpg" alt="" /></a>
+                </article>
+
+            </div>
+        </section>
+
+        <!-- Posts List -->
+        <section>
+            <ul class="posts">
+                <li>
+                    <article>
+                        <header>
+                            <h3><a href="single.html">Lorem ipsum fermentum ut nisl vitae</a></h3>
+                            <time class="published" datetime="2015-10-20">October 20, 2015</time>
+                        </header>
+                        <a href="single.html" class="image"><img src="public/images/pic08.jpg" alt="" /></a>
+                    </article>
+                </li>
+                <li>
+                    <article>
+                        <header>
+                            <h3><a href="single.html">Convallis maximus nisl mattis nunc id lorem</a></h3>
+                            <time class="published" datetime="2015-10-15">October 15, 2015</time>
+                        </header>
+                        <a href="single.html" class="image"><img src="public/images/pic09.jpg" alt="" /></a>
+                    </article>
+                </li>
+                <li>
+                    <article>
+                        <header>
+                            <h3><a href="single.html">Euismod amet placerat vivamus porttitor</a></h3>
+                            <time class="published" datetime="2015-10-10">October 10, 2015</time>
+                        </header>
+                        <a href="single.html" class="image"><img src="public/images/pic10.jpg" alt="" /></a>
+                    </article>
+                </li>
+                <li>
+                    <article>
+                        <header>
+                            <h3><a href="single.html">Magna enim accumsan tortor cursus ultricies</a></h3>
+                            <time class="published" datetime="2015-10-08">October 8, 2015</time>
+                        </header>
+                        <a href="single.html" class="image"><img src="public/images/pic11.jpg" alt="" /></a>
+                    </article>
+                </li>
+                <li>
+                    <article>
+                        <header>
+                            <h3><a href="single.html">Congue ullam corper lorem ipsum dolor</a></h3>
+                            <time class="published" datetime="2015-10-06">October 7, 2015</time>
+                        </header>
+                        <a href="single.html" class="image"><img src="public/images/pic12.jpg" alt="" /></a>
+                    </article>
+                </li>
+            </ul>
+        </section>
+
+        <!-- About -->
+        <section class="blurb">
+            <h2>About</h2>
+            <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod amet placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at phasellus sed ultricies.</p>
+            <ul class="actions">
+                <li><a href="#" class="button">Learn More</a></li>
+            </ul>
+        </section>
+
+        <!-- Footer -->
+        <section id="footer">
+            <ul class="icons">
+                <li><a href="#" class="fa-twitter"><span class="label">Twitter</span></a></li>
+                <li><a href="#" class="fa-facebook"><span class="label">Facebook</span></a></li>
+                <li><a href="#" class="fa-instagram"><span class="label">Instagram</span></a></li>
+                <li><a href="#" class="fa-rss"><span class="label">RSS</span></a></li>
+                <li><a href="#" class="fa-envelope"><span class="label">Email</span></a></li>
+            </ul>
+            <p class="copyright">&copy; MyBlog <a href="#">Blog.WebVaha</a>. MySite: <a href="#">WebVaha</a>.</p>
+        </section>
+    </section>
 </div>
-
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
 
 <?php $this->endBody() ?>
 </body>
