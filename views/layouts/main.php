61aecbaab456d22a8e2b05900426568181bfac4d
Vaharsolta <vaha-22@mail.ru>
2018-12-04 10:06:47 +0300

    Вынес все по шаблонам

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -107,93 +107,8 @@ PublicAsset::register($this);
 
     </section>
 
-    <!-- Main -->
     <div id="main">
-
-        <!-- Post -->
-        <article class="post">
-            <header>
-                <div class="title">
-                    <h2><a href="single.html">Magna sed adipiscing</a></h2>
-                    <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
-                </div>
-                <div class="meta">
-                    <time class="published" datetime="2015-11-01">November 1, 2015</time>
-                    <a href="#" class="author"><span class="name">Jane Doe</span><img src="public/images/avatar.jpg" alt="" /></a>
-                </div>
-            </header>
-            <a href="single.html" class="image featured"><img src="public/images/pic01.jpg" alt="" /></a>
-            <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
-            <footer>
-                <ul class="actions">
-                    <li><a href="single.html" class="button large">Continue Reading</a></li>
-                </ul>
-                <ul class="stats">
-                    <li><a href="#">General</a></li>
-                    <li><a href="#" class="icon fa-heart">28</a></li>
-                    <li><a href="#" class="icon fa-comment">128</a></li>
-                </ul>
-            </footer>
-        </article>
-
-        <!-- Post -->
-        <article class="post">
-            <header>
-                <div class="title">
-                    <h2><a href="single.html">Ultricies sed magna euismod enim vitae gravida</a></h2>
-                    <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
-                </div>
-                <div class="meta">
-                    <time class="published" datetime="2015-10-25">October 25, 2015</time>
-                    <a href="#" class="author"><span class="name">Jane Doe</span><img src="public/images/avatar.jpg" alt="" /></a>
-                </div>
-            </header>
-            <a href="single.html" class="image featured"><img src="public/images/pic02.jpg" alt="" /></a>
-            <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper.</p>
-            <footer>
-                <ul class="actions">
-                    <li><a href="single.html" class="button large">Continue Reading</a></li>
-                </ul>
-                <ul class="stats">
-                    <li><a href="#">General</a></li>
-                    <li><a href="#" class="icon fa-heart">28</a></li>
-                    <li><a href="#" class="icon fa-comment">128</a></li>
-                </ul>
-            </footer>
-        </article>
-
-        <!-- Post -->
-        <article class="post">
-            <header>
-                <div class="title">
-                    <h2><a href="single.html">Euismod et accumsan</a></h2>
-                    <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
-                </div>
-                <div class="meta">
-                    <time class="published" datetime="2015-10-22">October 22, 2015</time>
-                    <a href="#" class="author"><span class="name">Jane Doe</span><img src="public/images/avatar.jpg" alt="" /></a>
-                </div>
-            </header>
-            <a href="single.html" class="image featured"><img src="public/images/pic03.jpg" alt="" /></a>
-            <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla. Cras vehicula tellus eu ligula viverra, ac fringilla turpis suscipit. Quisque vestibulum rhoncus ligula.</p>
-            <footer>
-                <ul class="actions">
-                    <li><a href="single.html" class="button large">Continue Reading</a></li>
-                </ul>
-                <ul class="stats">
-                    <li><a href="#">General</a></li>
-                    <li><a href="#" class="icon fa-heart">28</a></li>
-                    <li><a href="#" class="icon fa-comment">128</a></li>
-                </ul>
-            </footer>
-        </article>
-
-        <!-- Pagination -->
-        <ul class="actions pagination">
-            <li><a href="" class="disabled button large previous">Previous Page</a></li>
-            <li><a href="#" class="button large next">Next Page</a></li>
-        </ul>
-
+        <?php echo $content; ?>
     </div>
 
     <!-- Sidebar -->
@@ -201,7 +116,7 @@ PublicAsset::register($this);
 
         <!-- Intro -->
         <section id="intro">
-            <a href="#" class="logo"><img src="public/images/logo.jpg" alt="" /></a>
+            <a href="#" class="logo"><img src="/public/images/logo.jpg" alt="" /></a>
             <header>
                 <h2>Future Imperfect</h2>
                 <p>Another fine responsive site template by <a href="http://html5up.net">HTML5 UP</a></p>
@@ -217,9 +132,9 @@ PublicAsset::register($this);
                     <header>
                         <h3><a href="single.html">Vitae sed condimentum</a></h3>
                         <time class="published" datetime="2015-10-20">October 20, 2015</time>
-                        <a href="#" class="author"><img src="public/images/avatar.jpg" alt="" /></a>
+                        <a href="#" class="author"><img src="/public/images/avatar.jpg" alt="" /></a>
                     </header>
-                    <a href="single.html" class="image"><img src="public/images/pic04.jpg" alt="" /></a>
+                    <a href="single.html" class="image"><img src="/public/images/pic04.jpg" alt="" /></a>
                 </article>
 
                 <!-- Mini Post -->
@@ -227,9 +142,9 @@ PublicAsset::register($this);
                     <header>
                         <h3><a href="single.html">Rutrum neque accumsan</a></h3>
                         <time class="published" datetime="2015-10-19">October 19, 2015</time>
-                        <a href="#" class="author"><img src="public/images/avatar.jpg" alt="" /></a>
+                        <a href="#" class="author"><img src="/public/images/avatar.jpg" alt="" /></a>
                     </header>
-                    <a href="single.html" class="image"><img src="public/images/pic05.jpg" alt="" /></a>
+                    <a href="single.html" class="image"><img src="/public/images/pic05.jpg" alt="" /></a>
                 </article>
 
                 <!-- Mini Post -->
@@ -237,9 +152,9 @@ PublicAsset::register($this);
                     <header>
                         <h3><a href="single.html">Odio congue mattis</a></h3>
                         <time class="published" datetime="2015-10-18">October 18, 2015</time>
-                        <a href="#" class="author"><img src="public/images/avatar.jpg" alt="" /></a>
+                        <a href="#" class="author"><img src="/public/images/avatar.jpg" alt="" /></a>
                     </header>
-                    <a href="single.html" class="image"><img src="public/images/pic06.jpg" alt="" /></a>
+                    <a href="single.html" class="image"><img src="/public/images/pic06.jpg" alt="" /></a>
                 </article>
 
                 <!-- Mini Post -->
@@ -247,9 +162,9 @@ PublicAsset::register($this);
                     <header>
                         <h3><a href="single.html">Enim nisl veroeros</a></h3>
                         <time class="published" datetime="2015-10-17">October 17, 2015</time>
-                        <a href="#" class="author"><img src="public/images/avatar.jpg" alt="" /></a>
+                        <a href="#" class="author"><img src="/public/images/avatar.jpg" alt="" /></a>
                     </header>
-                    <a href="single.html" class="image"><img src="public/images/pic07.jpg" alt="" /></a>
+                    <a href="single.html" class="image"><img src="/public/images/pic07.jpg" alt="" /></a>
                 </article>
 
             </div>
@@ -264,7 +179,7 @@ PublicAsset::register($this);
                             <h3><a href="single.html">Lorem ipsum fermentum ut nisl vitae</a></h3>
                             <time class="published" datetime="2015-10-20">October 20, 2015</time>
                         </header>
-                        <a href="single.html" class="image"><img src="public/images/pic08.jpg" alt="" /></a>
+                        <a href="single.html" class="image"><img src="/public/images/pic08.jpg" alt="" /></a>
                     </article>
                 </li>
                 <li>
@@ -273,7 +188,7 @@ PublicAsset::register($this);
                             <h3><a href="single.html">Convallis maximus nisl mattis nunc id lorem</a></h3>
                             <time class="published" datetime="2015-10-15">October 15, 2015</time>
                         </header>
-                        <a href="single.html" class="image"><img src="public/images/pic09.jpg" alt="" /></a>
+                        <a href="single.html" class="image"><img src="/public/images/pic09.jpg" alt="" /></a>
                     </article>
                 </li>
                 <li>
@@ -282,7 +197,7 @@ PublicAsset::register($this);
                             <h3><a href="single.html">Euismod amet placerat vivamus porttitor</a></h3>
                             <time class="published" datetime="2015-10-10">October 10, 2015</time>
                         </header>
-                        <a href="single.html" class="image"><img src="public/images/pic10.jpg" alt="" /></a>
+                        <a href="single.html" class="image"><img src="/public/images/pic10.jpg" alt="" /></a>
                     </article>
                 </li>
                 <li>
@@ -291,7 +206,7 @@ PublicAsset::register($this);
                             <h3><a href="single.html">Magna enim accumsan tortor cursus ultricies</a></h3>
                             <time class="published" datetime="2015-10-08">October 8, 2015</time>
                         </header>
-                        <a href="single.html" class="image"><img src="public/images/pic11.jpg" alt="" /></a>
+                        <a href="single.html" class="image"><img src="/public/images/pic11.jpg" alt="" /></a>
                     </article>
                 </li>
                 <li>
@@ -300,7 +215,7 @@ PublicAsset::register($this);
                             <h3><a href="single.html">Congue ullam corper lorem ipsum dolor</a></h3>
                             <time class="published" datetime="2015-10-06">October 7, 2015</time>
                         </header>
-                        <a href="single.html" class="image"><img src="public/images/pic12.jpg" alt="" /></a>
+                        <a href="single.html" class="image"><img src="/public/images/pic12.jpg" alt="" /></a>
                     </article>
                 </li>
             </ul>
