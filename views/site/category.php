dba92bd37cc3573d3c60c23b0ab7cf983b8e39c8
Vaharsolta <vaha-22@mail.ru>
2018-12-05 19:31:48 +0300

    Шаблон для страницы категории

---
+++ views/site/category.php
@@ -0,0 +1,75 @@
+<!-- Post -->
+<article class="post">
+    <header>
+        <div class="title">
+            <h2><a href="single.html">Magna sed adipiscing</a></h2>
+            <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
+        </div>
+        <div class="meta">
+            <time class="published" datetime="2015-11-01">November 1, 2015</time>
+            <a href="#" class="author"><span class="name">Jane Doe</span><img src="/public/images/avatar.jpg" alt="" /></a>
+        </div>
+    </header>
+
+    <footer>
+        <ul class="actions">
+            <li><a href="single.html" class="button large">Continue Reading</a></li>
+        </ul>
+        <ul class="stats">
+            <li><a href="#">General</a></li>
+            <li><a href="#" class="icon fa-heart">28</a></li>
+            <li><a href="#" class="icon fa-comment">128</a></li>
+        </ul>
+    </footer>
+</article>
+
+<!-- Post -->
+<article class="post">
+    <header>
+        <div class="title">
+            <h2><a href="single.html">Ultricies sed magna euismod enim vitae gravida</a></h2>
+            <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
+        </div>
+        <div class="meta">
+            <time class="published" datetime="2015-10-25">October 25, 2015</time>
+            <a href="#" class="author"><span class="name">Jane Doe</span><img src="/public/images/avatar.jpg" alt="" /></a>
+        </div>
+    </header>
+
+    <footer>
+        <ul class="actions">
+            <li><a href="single.html" class="button large">Continue Reading</a></li>
+        </ul>
+        <ul class="stats">
+            <li><a href="#">General</a></li>
+            <li><a href="#" class="icon fa-heart">28</a></li>
+            <li><a href="#" class="icon fa-comment">128</a></li>
+        </ul>
+    </footer>
+</article>
+
+<!-- Post -->
+<article class="post">
+    <header>
+        <div class="title">
+            <h2><a href="single.html">Euismod et accumsan</a></h2>
+            <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
+        </div>
+        <div class="meta">
+            <time class="published" datetime="2015-10-25">October 25, 2015</time>
+            <a href="#" class="author"><span class="name">Jane Doe</span><img src="/public/images/avatar.jpg" alt="" /></a>
+        </div>
+    </header>
+    <footer>
+        <ul class="actions">
+            <li><a href="single.html" class="button large">Continue Reading</a></li>
+        </ul>
+        <ul class="stats">
+            <li><a href="#">General</a></li>
+            <li><a href="#" class="icon fa-heart">28</a></li>
+            <li><a href="#" class="icon fa-comment">128</a></li>
+        </ul>
+    </footer>
+</article>
+
+
