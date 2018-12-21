f4c858ac07081a19acf40eda3918589ae907fe6e
Vaharsolta <vaha-22@mail.ru>
2018-12-21 21:03:52 +0300

    Вывожу список статей цыклом

--- views/site/index.php
+++ views/site/index.php
@@ -4,87 +4,30 @@
 
 $this->title = 'My Yii Application';
 ?>
-
-<!-- Main -->
-
-
-    <!-- Post -->
-    <article class="post">
-        <header>
-            <div class="title">
-                <h2><a href="single.html">Magna sed adipiscing</a></h2>
-                <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
-            </div>
-            <div class="meta">
-                <time class="published" datetime="2015-11-01">November 1, 2015</time>
-                <a href="#" class="author"><span class="name">Jane Doe</span><img src="/public/images/avatar.jpg" alt="" /></a>
-            </div>
-        </header>
-        <a href="single.html" class="image featured"><img src="/public/images/pic01.jpg" alt="" /></a>
-        <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
-        <footer>
-            <ul class="actions">
-                <li><a href="single.html" class="button large">Continue Reading</a></li>
-            </ul>
-            <ul class="stats">
-                <li><a href="#">General</a></li>
-                <li><a href="#" class="icon fa-heart">28</a></li>
-                <li><a href="#" class="icon fa-comment">128</a></li>
-            </ul>
-        </footer>
-    </article>
-
-    <!-- Post -->
-    <article class="post">
-        <header>
-            <div class="title">
-                <h2><a href="single.html">Ultricies sed magna euismod enim vitae gravida</a></h2>
-                <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
-            </div>
-            <div class="meta">
-                <time class="published" datetime="2015-10-25">October 25, 2015</time>
-                <a href="#" class="author"><span class="name">Jane Doe</span><img src="/public/images/avatar.jpg" alt="" /></a>
-            </div>
-        </header>
-        <a href="single.html" class="image featured"><img src="/public/images/pic02.jpg" alt="" /></a>
-        <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper.</p>
-        <footer>
-            <ul class="actions">
-                <li><a href="single.html" class="button large">Continue Reading</a></li>
-            </ul>
-            <ul class="stats">
-                <li><a href="#">General</a></li>
-                <li><a href="#" class="icon fa-heart">28</a></li>
-                <li><a href="#" class="icon fa-comment">128</a></li>
-            </ul>
-        </footer>
-    </article>
-
-    <!-- Post -->
-    <article class="post">
-        <header>
-            <div class="title">
-                <h2><a href="single.html">Euismod et accumsan</a></h2>
-                <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
-            </div>
-            <div class="meta">
-                <time class="published" datetime="2015-10-22">October 22, 2015</time>
-                <a href="#" class="author"><span class="name">Jane Doe</span><img src="/public/images/avatar.jpg" alt="" /></a>
-            </div>
-        </header>
-        <a href="single.html" class="image featured"><img src="/public/images/pic03.jpg" alt="" /></a>
-        <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla. Cras vehicula tellus eu ligula viverra, ac fringilla turpis suscipit. Quisque vestibulum rhoncus ligula.</p>
-        <footer>
-            <ul class="actions">
-                <li><a href="single.html" class="button large">Continue Reading</a></li>
-            </ul>
-            <ul class="stats">
-                <li><a href="#">General</a></li>
-                <li><a href="#" class="icon fa-heart">28</a></li>
-                <li><a href="#" class="icon fa-comment">128</a></li>
-            </ul>
-        </footer>
-    </article>
+    <?php foreach ($articles as $article) : ?>
+        <article class="post">
+            <header>
+                <div class="title">
+                    <h2><a href="single.html"><?php echo $article->title; ?></a></h2>
+                    <p><?php echo $article->description; ?></p>
+                </div>
+                <div class="meta">
+                    <time class="published" datetime="2015-11-01"><?php echo $article->date; ?></time>
+                    <a href="#" class="author"><span class="name">Jane Doe</span><img src="/public/images/avatar.jpg" alt="" /></a>
+                </div>
+            </header>
+            <a href="single.html" class="image featured"><img src="/public/images/pic01.jpg" alt="" /></a>
+            <p><?php echo $article->content; ?></p>
+            <footer>
+                <ul class="actions">
+                    <li><a href="single.html" class="button large">Continue Reading</a></li>
+                </ul>
+                <ul class="stats">
+                    <li><a href="#" class="icon fa-eye"><?php echo (int)$article->viewed; ?></a></li>
+                </ul>
+            </footer>
+        </article>
+    <?php endforeach; ?>
 
     <!-- Pagination -->
     <ul class="actions pagination">
