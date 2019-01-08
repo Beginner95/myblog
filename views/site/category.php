ca5e45f827c8ce17a8b80afed9f62edc89670498
Vaharsolta <vaha-22@mail.ru>
2019-01-08 18:24:02 +0300

    Реализовал вид списка статей в категории

--- views/site/category.php
+++ views/site/category.php
@@ -1,75 +1,70 @@
-<!-- Post -->
-<article class="post">
-    <header>
-        <div class="title">
-            <h2><a href="single.html">Magna sed adipiscing</a></h2>
-            <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
-        </div>
-        <div class="meta">
-            <time class="published" datetime="2015-11-01">November 1, 2015</time>
-            <a href="#" class="author"><span class="name">Jane Doe</span><img src="/public/images/avatar.jpg" alt="" /></a>
-        </div>
-    </header>
+<?php
 
-    <footer>
-        <ul class="actions">
-            <li><a href="single.html" class="button large">Continue Reading</a></li>
-        </ul>
-        <ul class="stats">
-            <li><a href="#">General</a></li>
-            <li><a href="#" class="icon fa-heart">28</a></li>
-            <li><a href="#" class="icon fa-comment">128</a></li>
-        </ul>
-    </footer>
-</article>
+/* @var $this yii\web\View */
 
-<!-- Post -->
-<article class="post">
-    <header>
-        <div class="title">
-            <h2><a href="single.html">Ultricies sed magna euismod enim vitae gravida</a></h2>
-            <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
-        </div>
-        <div class="meta">
-            <time class="published" datetime="2015-10-25">October 25, 2015</time>
-            <a href="#" class="author"><span class="name">Jane Doe</span><img src="/public/images/avatar.jpg" alt="" /></a>
-        </div>
-    </header>
+use yii\helpers\Url;
+use yii\widgets\LinkPager;
 
-    <footer>
-        <ul class="actions">
-            <li><a href="single.html" class="button large">Continue Reading</a></li>
-        </ul>
-        <ul class="stats">
-            <li><a href="#">General</a></li>
-            <li><a href="#" class="icon fa-heart">28</a></li>
-            <li><a href="#" class="icon fa-comment">128</a></li>
-        </ul>
-    </footer>
-</article>
+$this->title = 'My Yii Application';
+?>
+<div id="main">
+    <?php foreach ($articles as $article) : ?>
+        <article class="post">
+            <header>
+                <div class="title">
+                    <h2><a href="<?php echo Url::toRoute(['site/view', 'id' => $article->id]); ?>"><?php echo $article->title; ?></a></h2>
+                    <p><?php echo $article->description; ?></p>
+                </div>
+                <div class="meta">
+                    <time class="published" datetime="<?php echo $article->date; ?>"><?php echo $article->getDate(); ?></time>
+                    <a href="#" class="author"><span class="name">Jane Doe</span><img src="/public/images/avatar.jpg" alt="" /></a>
+                </div>
+            </header>
+            <a href="<?php echo Url::toRoute(['site/view', 'id' => $article->id]); ?>" class="image featured"><img src="<?php echo $article->getImage(); ?>" alt="" /></a>
+            <p><?php echo $article->content; ?></p>
+            <footer>
+                <ul class="actions">
+                    <li><a href="<?php echo Url::toRoute(['site/view', 'id' => $article->id]); ?>" class="button large">Continue Reading</a></li>
+                </ul>
+                <ul class="stats">
+                    <li><a href="#" class="icon fa-eye"><?php echo (int)$article->viewed; ?></a></li>
+                </ul>
+            </footer>
+        </article>
+    <?php endforeach; ?>
+    <?php
+    echo LinkPager::widget([
+        'pagination' => $pagination,
+        //Css option for container
+        'options' => ['class' => 'actions pagination'],
+        //First option value
+        //    'firstPageLabel' => 'first',
+        //Last option value
+        //    'lastPageLabel' => 'last',
+        //Previous option value
+        'prevPageLabel' => 'PREVIOUS PAGE',
+        //Next option value
+        'nextPageLabel' => 'Next Page',
+        //Current Active option value
+        'activePageCssClass' => 'active',
+        //Max count of allowed options
+        //    'maxButtonCount' => 8,
+        // Css for each options. Links
+        'linkOptions' => ['class' => 'button '],
+        //    'disabledPageCssClass' => '',
+        // Customzing CSS class for navigating link
+        //    'prevPageCssClass' => '',
+        //    'nextPageCssClass' => '',
+        //    'firstPageCssClass' => 'p-first',
+        //    'lastPageCssClass' => 'p-last',
+    ]);
+    ?>
+</div>
 
-<!-- Post -->
-<article class="post">
-    <header>
-        <div class="title">
-            <h2><a href="single.html">Euismod et accumsan</a></h2>
-            <p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
-        </div>
-        <div class="meta">
-            <time class="published" datetime="2015-10-25">October 25, 2015</time>
-            <a href="#" class="author"><span class="name">Jane Doe</span><img src="/public/images/avatar.jpg" alt="" /></a>
-        </div>
-    </header>
-    <footer>
-        <ul class="actions">
-            <li><a href="single.html" class="button large">Continue Reading</a></li>
-        </ul>
-        <ul class="stats">
-            <li><a href="#">General</a></li>
-            <li><a href="#" class="icon fa-heart">28</a></li>
-            <li><a href="#" class="icon fa-comment">128</a></li>
-        </ul>
-    </footer>
-</article>
+<?php
 
-
+echo $this->render('/partials/sidebar', [
+    'popular' => $popular,
+    'recent' => $recent,
+    'categories' => $categories
+]);
