fbc6d47bf3214da7b27232db85c4513e9d708492
Vaharsolta <vaha-22@mail.ru>
2019-01-13 14:27:13 +0300

    Создал класс авторизации

---
+++ controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\User;
+use Yii;
+use yii\web\Controller;
+use app\models\LoginForm;
+
+class AuthController extends Controller
+{
+    /**
+     * Login action.
+     *
+     * @return string
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+        return $this->render('/site/login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return string
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+}
