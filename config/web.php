c1fb1ddd7e4c22f4b460cf876907675cc60e7b79
Vaharsolta <vaha-22@mail.ru>
2019-01-13 14:02:39 +0300

    Удалил ненужные компоненты

--- config/web.php
+++ config/web.php
@@ -16,9 +16,7 @@ $config = [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'vNtQ-Uk9QVFOXzMJ1R8rTXLAHku7c2VI',
         ],
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
+
         'user' => [
             'identityClass' => 'app\models\User',
             'enableAutoLogin' => true,
@@ -26,22 +24,7 @@ $config = [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
+
         'db' => $db,
         
         'urlManager' => [
