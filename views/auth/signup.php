d41943386dfd972619414b013b3a12012718dfd2
Vaharsolta <vaha-22@mail.ru>
2019-01-14 13:47:08 +0300

    Реализовал шаблон регистрации

---
+++ views/auth/signup.php
@@ -0,0 +1,42 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model app\models\LoginForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+$this->title = 'Register';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-register">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Please fill in the registration fields:</p>
+
+    <?php $form = ActiveForm::begin([
+        'id' => 'login-form',
+        'layout' => 'horizontal',
+        'fieldConfig' => [
+            'template' => "{label}\n<div class=\"col-lg-1\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
+            'labelOptions' => ['class' => 'col-lg-1 control-label'],
+        ],
+    ]); ?>
+
+    <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput() ?>
+
+    <?= $form->field($model, 'password')->passwordInput() ?>
+
+    <div class="form-group">
+        <div class="col-lg-offset-1 col-lg-11">
+            <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+        </div>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+
+</div>
