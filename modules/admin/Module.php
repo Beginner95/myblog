923cac71296b5d62c8d506686516aea1cc5e803b
Vaharsolta <vaha-22@mail.ru>
2019-01-16 21:07:16 +0300

    Создал метод поведения для сокрытия админки

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -1,12 +1,38 @@
 <?php
 
 namespace app\modules\admin;
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\NotFoundHttpException;
 
 /**
  * admin module definition class
  */
 class Module extends \yii\base\Module
 {
+
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'denyCallback' => function($rule, $action)
+                {
+                    throw new NotFoundHttpException();
+                },
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'matchCallback' => function($rule, $action)
+                        {
+                            return Yii::$app->user->identity->isAdmin;
+                        }
+                    ]
+                ]
+            ]
+        ];
+    }
+
     /**
      * @inheritdoc
      */
