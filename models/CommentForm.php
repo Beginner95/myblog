4fad2088d29ec016da8524d43c452a0ed2c7e1cd
Vaharsolta <vaha-22@mail.ru>
2019-01-18 22:31:48 +0300

    Создал метод по сохранению комментария

--- models/CommentForm.php
+++ models/CommentForm.php
@@ -3,7 +3,10 @@
 namespace app\models;
 
 
-class CommentForm
+use Yii;
+use yii\base\Model;
+
+class CommentForm extends Model
 {
     public $comment;
 
@@ -13,5 +16,15 @@ class CommentForm
             [['comment'], 'required'],
             [['comment'], 'string', 'length' => [3, 250]]
         ];
+    }
+
+    public function saveComment($article_id)
+    {
+        $comment = new Comment();
+        $comment->text = $this->comment;
+        $comment->user_id = Yii::$app->user->id;
+        $comment->article_id = $article_id;
+        $comment->status = 0;
+        return $comment->save();
     }
 }
