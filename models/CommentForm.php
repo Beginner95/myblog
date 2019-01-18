ee4bc8dede5558d7a92f55bdfbbeac0d256730cf
Vaharsolta <vaha-22@mail.ru>
2019-01-18 21:44:50 +0300

    Реализовал метод для добавления комментариев

---
+++ models/CommentForm.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\models;
+
+
+class CommentForm
+{
+    public $comment;
+
+    public function rules()
+    {
+        return [
+            [['comment'], 'required'],
+            [['comment'], 'string', 'length' => [3, 250]]
+        ];
+    }
+}
