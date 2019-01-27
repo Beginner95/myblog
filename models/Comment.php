8810bb4ec6f17de8aa1f31b6beb4d928e3a7b374
Vaharsolta <vaha-22@mail.ru>
2019-01-27 06:52:43 +0300

    Завел константы для локаничности кода

--- models/Comment.php
+++ models/Comment.php
@@ -21,6 +21,9 @@ class Comment extends \yii\db\ActiveRecord
     /**
      * @inheritdoc
      */
+    const STATUS_ALLOW = 1;
+    const STATUS_DISALLOW = 0;
+
     public static function tableName()
     {
         return 'comment';
@@ -90,13 +93,13 @@ class Comment extends \yii\db\ActiveRecord
 
     public function allow()
     {
-        $this->status = 1;
+        $this->status = self::STATUS_ALLOW;
         return $this->save(false);
     }
 
     public function disallow()
     {
-        $this->status = 0;
+        $this->status = self::STATUS_DISALLOW;
         return $this->save(false);
     }
 
