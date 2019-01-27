20512897bf30a6345d6807f53983374189d79f6b
Vaharsolta <vaha-22@mail.ru>
2019-01-27 07:06:23 +0300

    Создал метод получения автора статьи

--- models/Article.php
+++ models/Article.php
@@ -182,4 +182,8 @@ class Article extends \yii\db\ActiveRecord
         return $this->getComments()->where(['status' => 1])->all();
     }
 
+    public function getAuthor()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
 }
