dbc6d9023231c873efbdbbc22b8b63429bdd264e
Vaharsolta <vaha-22@mail.ru>
2019-02-02 21:55:01 +0300

    Вид для статей найденных по тегу

---
+++ views/site/tag.php
@@ -0,0 +1,43 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Url;
+use yii\widgets\LinkPager;
+
+$this->title = 'My Yii Application';
+?>
+    <div id="main">
+        <?php foreach ($articles as $article) : ?>
+            <article class="post">
+                <header>
+                    <div class="title">
+                        <h2><a href="<?php echo Url::toRoute(['site/view', 'id' => $article->id]); ?>"><?php echo $article->title; ?></a></h2>
+                        <p><?php echo $article->description; ?></p>
+                    </div>
+                    <div class="meta">
+                        <time class="published" datetime="<?php echo $article->date; ?>"><?php echo $article->getDate(); ?></time>
+                        <a href="#" class="author"><span class="name">Jane Doe</span><img src="/public/images/avatar.jpg" alt="" /></a>
+                    </div>
+                </header>
+                <a href="<?php echo Url::toRoute(['site/view', 'id' => $article->id]); ?>" class="image featured"><img src="<?php echo $article->getImage(); ?>" alt="" /></a>
+                <p><?php echo $article->content; ?></p>
+                <footer>
+                    <ul class="actions">
+                        <li><a href="<?php echo Url::toRoute(['site/view', 'id' => $article->id]); ?>" class="button large">Continue Reading</a></li>
+                    </ul>
+                    <ul class="stats">
+                        <li><a href="#" class="icon fa-eye"><?php echo (int)$article->viewed; ?></a></li>
+                    </ul>
+                </footer>
+            </article>
+        <?php endforeach; ?>
+    </div>
+
+<?php
+
+echo $this->render('/partials/sidebar', [
+    'popular' => $popular,
+    'recent' => $recent,
+    'categories' => $categories
+]);
