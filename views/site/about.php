471b023899bb571e61d8859d7278f35cd35b0520
Vaharsolta <vaha-22@mail.ru>
2019-02-08 11:08:56 +0300

    Реализовал вид для страницы обо мне

--- views/site/about.php
+++ views/site/about.php
@@ -7,12 +7,46 @@ use yii\helpers\Html;
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+<div id="main">
+    <article class="post">
+        <header>
+            <div class="title">
+                <h2><?= Html::encode($this->title) ?></h2>
+            </div>
+        </header>
+            <section>
+                <p>My name is Vaharsolta Nesirkhayev; I do web programming. This blog is created for yourself. I will publish a record of solving problems in the field of programming they will serve as a sharper in the first place for me</p>
+                <p>My skills:</p>
+                <ul class="links">
+                    <li>
+                        <h3>HTML</h3>
+                        <p>Average level (IDE Sublime Text + emmet)</p>
+                    </li>
+                    <li>
+                        <h3>CSS</h3>
+                        <p>Bootstrap (IDE Sublime Text)</p>
+                    </li>
+                    <li>
+                        <h3>JavaScript</h3>
+                        <p>Vue, React (IDE Sublime Text)</p>
+                    </li>
+                    <li>
+                        <h3>PHP</h3>
+                        <p>Yii2, Laravel (IDE PhpStorm)</p>
+                    </li>
+                    <li>
+                        <h3>MySQL</h3>
+                        <p>(IDE PhpMyAdmin and Workbench)</p>
+                    </li>
+                </ul>
+                <p>In my free time I like electronics, 3D design. I also have a great interest in thermodynamics.</p>
+            </section>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
-
-    <code><?= __FILE__ ?></code>
+    </article>
 </div>
+<?php
+echo $this->render('/partials/sidebar', [
+    'popular' => $popular,
+    'recent' => $recent,
+    'categories' => $categories
+]);
