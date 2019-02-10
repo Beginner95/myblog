a9d347c6bfb6122623c8f9f6504d6e5b105eb7ce
Vaharsolta <vaha-22@mail.ru>
2019-02-10 06:36:56 +0300

    Установил проверку существовании категории при установке новой

--- modules/admin/controllers/ArticleController.php
+++ modules/admin/controllers/ArticleController.php
@@ -152,7 +152,7 @@ class ArticleController extends Controller
     public function actionSetCategory($id)
     {
         $article = $this->findModel($id);
-        $selectedCategory = $article->category->id;
+        $selectedCategory = !empty($article->category->id) ? $article->category->id : 0;
         $categories = ArrayHelper::map(Category::find()->all(), 'id', 'title');
 
         if (true === Yii::$app->request->isPost) {
