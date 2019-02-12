1fd4a4616132accb5c4ab43474b7603028d1a431
Vaharsolta <vaha-22@mail.ru>
2019-02-12 09:20:03 +0300

    Указал контроллер по умолчанию

--- config/web.php
+++ config/web.php
@@ -8,6 +8,7 @@ $config = [
     'name' => 'Blog WebVaha',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'defaultRoute' => 'site/index',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
