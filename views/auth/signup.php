64ae741998d2a8e71e850d11c5822fa101173abe
Vaharsolta <vaha-22@mail.ru>
2019-02-15 06:54:07 +0300

    Добавил CAPTCHA к форм регистрации

--- views/auth/signup.php
+++ views/auth/signup.php
@@ -6,6 +6,7 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use yii\captcha\Captcha;
 
 $this->title = 'Register';
 $this->params['breadcrumbs'][] = $this->title;
@@ -30,9 +31,13 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?= $form->field($model, 'password')->passwordInput() ?>
 
+    <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
+        'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
+    ]) ?>
+
     <div class="form-group">
         <div class="col-lg-offset-1 col-lg-11">
-            <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            <?= Html::submitButton('Login up', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
         </div>
     </div>
 
