ee800de4935edecdf68ce05f1e93255d3b69a151
Vaharsolta <vaha-22@mail.ru>
2019-03-27 11:51:07 +0300

    Метод правил

--- models/ArticleCategory.php
+++ models/ArticleCategory.php
@@ -10,4 +10,14 @@ class ArticleCategory extends \yii\db\ActiveRecord
     {
         return 'article_to_category';
     }
+
+    public function rules()
+    {
+        return [
+            [['article_id', 'category_id'], 'integer'],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => Category::className(), 'targetAttribute' => ['category_id' => 'id']],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => Article::className(), 'targetAttribute' => ['article_id' => 'id']]
+
+        ];
+    }
 }
