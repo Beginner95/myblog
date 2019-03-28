c2276925d0117ab70aacbbe2395258ddb29ae0d3
Vaharsolta <vaha-22@mail.ru>
2019-03-28 11:54:52 +0300

    Реализована возможость выбора нескольких категорий для статьи

--- modules/admin/controllers/ArticleController.php
+++ modules/admin/controllers/ArticleController.php
@@ -149,17 +149,16 @@ class ArticleController extends Controller
         return $this->render('image', ['model' => $model]);
     }
 
-    public function actionSetCategory($id)
+    public function actionSetCategories($id)
     {
         $article = $this->findModel($id);
-        $selectedCategory = !empty($article->category->id) ? $article->category->id : 0;
+        $selectedCategory = $article->getSelectedCategories();
         $categories = ArrayHelper::map(Category::find()->all(), 'id', 'title');
 
-        if (true === Yii::$app->request->isPost) {
+        if (Yii::$app->request->isPost) {
             $category = Yii::$app->request->post('category');
-            if (true === $article->saveCategory($category)) {
-                return $this->redirect(['view', 'id' => $article->id]);
-            }
+            $article->saveCategory($category);
+            return $this->redirect(['view', 'id' => $article->id]);
         }
 
         return $this->render('category', [
